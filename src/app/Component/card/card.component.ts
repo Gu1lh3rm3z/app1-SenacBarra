981dd3e05685389627897e35379a916ed20aed3d
Técnico em Informática 2022.3 <62127512022.3@senacrjedu.local>
2023-12-01 16:55:27 -0300

    Login
    Aula- 02

--- src/app/Component/card/card.component.ts
+++ src/app/Component/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent  implements OnInit {
-
+  @Input() imagem:any=''
+  @Input() subTitulo:any=''
+  @Input() titulo:any=''
+  @Input() conteudo:any=''
+  @Input() botao:any=''
   constructor() { }
 
   ngOnInit() {}
